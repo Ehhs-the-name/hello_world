573bcb919f464b5d7bf2d4c486d16d429710706b
Ehh Uniy <keffendi2010@gmail.com>
2024-08-26 15:02:23 -0400

    added lil Java message :)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("\n.+' Hello World '+.\n");
+      System.out.println("\n.+' Hello World '+.\nLearning Java's gonna be a ride...\n");
     }
   }
   
